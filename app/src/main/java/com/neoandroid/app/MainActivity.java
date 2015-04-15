52b24ae87ff12470faeaa21868e2edc9204b7cec
francis.malit <francis.malit@aestrea.com>
2015-04-15 22:25:13 +0800

    testing textView

--- app/src/main/java/com/neoandroid/app/MainActivity.java
+++ app/src/main/java/com/neoandroid/app/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +13,9 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText editText = (EditText)findViewById(R.id.editText);
+        editText.setText("pulpol");
     }
 
 
